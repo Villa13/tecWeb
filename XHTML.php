6dc1353570b219f28d081b8f0ccbb7bc5e26fdcf
Villa13 <isaacvillaluna@gmail.com>
2022-09-13 23:42:05 -0500

    Ejercicio1

---
+++ XHTML.php
@@ -0,0 +1,35 @@
+<?php
+$_myvar
+//Es valida por que comieza con un guion bajo seguido de una letra.
+$_7var
+//Es valida por que comienza con un guion bajo seguido de un numero.
+$myvar
+//Es valida por que comienza con una letra y es seguida tambien de una letra.
+$var7
+//Es valida por que comienza con una letra y es seguida tambien de una letra.
+$_elementl
+//Es valida por que comieza con un guion bajo seguido de una letra.
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
